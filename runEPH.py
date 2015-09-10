1b4252c94f3ffe761b91d08d052cfff251f6b8d3
Mark Dennehy <mark.dennehy@workday.com>
2015-09-10 23:14:22 +0100

    Adding in initial code, the json schema for prepareData's output and runEPH's input, and some sample and test data.

--- runEPH.py
+++ runEPH.py
@@ -1,4 +1,5 @@
 #!/usr/local/bin/python
+# This Python file uses the following encoding: utf-8
 ################################################################################
 #   Runs the E Pluribus Hugo algorithim on the given dataset                   #
 #   Copyright (C) 2015 Mark Dennehy                                            #
@@ -29,15 +30,90 @@ if __name__ == '__main__':
         print "Usage: runEPH.py filename.json\n"
         exit(1)
 
-    nominations = []
     datafile = sys.argv[1]
 
+    works = {}
+    categories = {}
+    ballots = []
+
     with open(datafile) as data_file:
-        nominations = json.load(data_file)
+        data = json.load(data_file)
+        for work in data['works']:
+            works[work['id']] = work['title']
+        for category in data['categories']:
+            categories[category['id']] = category['name']
+        for ballot in data['ballots']:
+            votes = []
+            for vote in ballot['votes']:
+                votes.append({'work':vote['work'],'preference':vote['preference']})
+            ballots.append({'category':ballot['category'], 'votes':votes})
 
-    print len(nominations)
+    works_by_category = {}
+    for category in iter(categories):
+        works_by_category[category] = set([])
 
-    for nomination in nominations:
-        print nomination
-        exit
+    for ballot in ballots:
+        for vote in ballot['votes']:
+            works_by_category[ballot['category']].add(vote['work'])
 
+    print "Data loaded. Running EPH."
+
+# Section 3.A: Finalist Selection Process
+# 3.A.1: For each category, the finalist selection process shall be conducted as
+# elimination rounds consisting of three phases:
+    for category in iter(categories):
+
+# (1) Calculation Phase: First, the total number of nominations (the number of
+# ballots on which each nominee appears) from all eligible ballots shall be
+# tallied for each remaining nominee. Next, a single “point” shall be assigned
+# to each nomination ballot. That point shall be divided equally among all
+# remaining nominees on that ballot. Finally, all points from all nomination
+# ballots shall be totaled for each nominee in that category. These two numbers,
+# point total and number of nominations, shall be used in the Selection and
+# Elimination Phases.
+        for ballot in (b for b in ballots if b['category']==category):
+            print ballot
+
+
+
+
+
+# (2) Selection Phase: The two nominees with the lowest point totals shall be
+# selected for comparison in the Elimination Phase. (See 3.A.3 for ties.)
+
+# (3) Elimination Phase: Nominees chosen in the Selection Phase shall be
+# compared, and the nominee with the fewest number of nominations shall be
+# eliminated and removed from all ballots for the Calculation Phase of all
+# subsequent rounds. (See 3.A.3 for ties.)
+
+# 3.A.2: The phases described in 3.A.1 are repeated in order for each category
+# until the number of finalists specified in 3.8.1 remain. If elimination would
+# reduce the number of finalists to fewer than the number specified in section
+# 3.8.1, then instead no nominees will be eliminated during that round, and all
+# remaining nominees shall appear on the final ballot, extending it if
+# necessary.
+
+# 3.A.3: Ties shall be handled as described below:
+
+# (1) During the Selection Phase, if two or more nominees are tied for the
+# lowest point total, all such nominees shall be selected for the Elimination
+# Phase.
+
+# (2) During the Selection Phase, if one nominee has the lowest point total and
+# two or more nominees are tied for the second-lowest point total, then all such
+# nominees shall be selected for the Elimination Phase.
+
+# (3) During the Elimination Phase, if two or more nominees are tied for the
+# fewest number of nominations, the nominee with the lowest point total at that
+# round shall be eliminated.
+
+# (4) During the Elimination Phase, if two or more nominees are tied for both
+# fewest number of nominations and lowest point total, then all such nominees
+# tied at that round shall be eliminated.
+
+# 3.A.4: After the initial Award ballot is generated, if any finalist(s) are
+# removed for any reason, the finalist selection process shall be rerun as
+# though the removed finalist(s) had never been nominee(s). None of the
+# remaining original finalists who have been notified shall be removed as a
+# result of this rerun. The new finalist(s) shall be merged with the original
+# finalists, extending the final ballot if necessary.
